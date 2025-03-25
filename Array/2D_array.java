2f9dfc16056b11635d1b20c9597a480723a54f3d
Darshan R <87401777+Darshan-5@users.noreply.github.com>
2025-03-25 22:37:44 +0530

    Create 2D_array.java

---
+++ Array/2D_array.java
@@ -0,0 +1,19 @@
+
+class Main {
+    public static void main(String[] args) {
+        
+        //int [][]a = new int[3][3];
+        int [][]b = {{1, 2, 3},
+                    {3, 4, 5},
+                    {4, 5, 6}
+    };
+    for (int i = 0;i<b.length;i++){
+        for(int j = 0;j<b.length;j++){
+        System.out.print(b[i][j]+ " ");
+        
+    }
+      System.out.println();  
+    }
+        }
+     }
+     
